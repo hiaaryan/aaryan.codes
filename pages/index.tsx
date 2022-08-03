ccecf4f338206a60e2920cae87c817362f2615c1
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2022-08-03 20:22:07 +0530

    fix ui 🎨

--- pages/index.tsx
+++ pages/index.tsx
@@ -14,19 +14,19 @@ const Home: NextPage = () => {
       <main className="w-full h-full flex items-center">
           <div>
             <div>
-              <div className="mb-6 lg:flex items-center">
+              <div className="mb-6 flex items-center">
                 <img className="w-14 lg:w-20 mr-6" src={'/image.png'} />
-                <h1 className="mt-6 lg:mt-0 text-gray-400 font-black text-3xl lg:text-5xl">
+                <h1 className="text-gray-400 font-black text-4xl lg:text-5xl">
                   hello, <br />
                   i am <span className="text-black">aaryan</span>.
                 </h1>
               </div>
               
-              <p className="text-xs lg:text-base">👨🏻‍💼 i am a digital marketer specializing in brand design and management. <div className="mt-2 lg:mt-0" /> 
-              👨🏻‍💻 i also develop webapps & native apps with php, js & tailwind.</p>
+              <div className="text-sm lg:text-base">👨🏻‍💼 i am a digital marketer specializing in brand design and management.</div>
+              <div className="hidden lg:block text-sm lg:text-base">👨🏻‍💻 i also develop webapps & native apps with php, js & tailwind.</div>
             </div>
 
-            <div className="text-xs lg:text-base absolute bottom-8 lg:bottom-16 flex justify-center gap-6">
+            <div className="text-xs lg:text-base absolute bottom-8 flex justify-center gap-6">
               <a href="https://www.linkedin.com/in/hiaaryan" target="_blank" rel="noreferrer" className="flex items-center text-gray-400 hover:text-black transition duration-150">
                 <IconBrandLinkedin className="w-6 mr-1.5 mb-0.5" stroke={2} /> LinkedIn
               </a>
