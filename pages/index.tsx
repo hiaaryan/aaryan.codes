4901d576b0161f7febb2576d58c268b971c39b43
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2022-08-12 14:40:58 +0530

    change everything 😅

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,31 +1,50 @@
 import { IconArrowUpRight, IconBrandGithub, IconBrandLinkedin } from '@tabler/icons'
+import { useState, useEffect } from 'react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 
 const Home: NextPage = () => {
+
+  const images = ['hand', 'heart', 'hey', 'okay'];
+  const [currentImage, setCurrentImage] = useState('');
+
+  useEffect(() => {
+      const intervalId = setInterval(() => {
+          setCurrentImage(images[Math.floor(Math.random() * images.length)]);
+      }, 1000)
+
+      return () => clearInterval(intervalId);
+  }, [])
+
   return (
-    <div className="h-screen w-screen font-mono p-4 lg:p-16">
+    <div className="font-mono bg-white h-screen w-screen">
       <Head>
-        <title>aaryan kapoor // digital marketer &amp; developer</title>
+        <title>aaryan kapoor // creative brand strategist</title>
         <meta name="description" content="🙋🏻‍♂️ hey there, i am aaryan. i am a developer, and a digital marketer specializing in brand design &amp; management." />
         <link rel="icon" href="/favicon.png" />
       </Head>
 
-      <main className="w-full h-full flex items-center">
+      <main className="flex items-center justify-center content h-screen p-4 lg:p-16 cursor-crosshair select-none">
           <div className="w-full">
-            <div className="w-full">
-              <div className="w-full mb-6 lg:flex items-center">
-                <div className="w-full lg:w-auto flex lg:flex-none justify-center">
-                  <img className="w-16 lg:w-20 lg:mr-6 text-center mb-6 lg:mb-0" src={'/image.png'} />
-                </div>
-                <h1 className="text-center lg:text-left text-gray-400 font-black text-4xl lg:text-5xl">
-                  hello, <br />
-                  i am <span className="text-black">aaryan</span>.
+            <div className="w-full flex-col text-center lg:text-left">
+              <div className="flex justify-center lg:justify-start">
+                <img alt="greeting image" className="w-16 animate-bounce" src={'/' + currentImage + '.png'} />
+              </div>
+              <div className="py-6 lg:max-w-xl">
+                <span className="text-base font-bold">
+                  hey i am,
+                </span>
+                <h1 className="mb-6 text-4xl lg:text-5xl font-black">
+                  aaryan kapoor.
                 </h1>
+                <p className="text-sm lg:text-base">
+                  a creative brand strategist who loves to design stuff &
+                  a self taught web developer specializing in php, js and css.
+                </p>
               </div>
             </div>
 
-            <div className="w-full lg:w-auto lg:gap-6 text-sm lg:text-base lg:absolute lg:bottom-8 flex justify-evenly lg:justify-start">
+            <div className="w-full lg:w-auto lg:gap-6 text-sm lg:text-base flex justify-evenly lg:justify-start">
               <a href="https://www.linkedin.com/in/hiaaryan" target="_blank" rel="noreferrer" className="flex items-center text-gray-400 hover:text-black transition duration-150">
                 <IconBrandLinkedin className="w-5 mr-1.5 mb-0.5" stroke={2} /> LinkedIn
               </a>
