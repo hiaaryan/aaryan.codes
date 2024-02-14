04e0cdaa81bb681fea7ba11762d5748e4afdc87e
Aaryan Kapoor <hi.aaryankapoor@gmail.com>
2024-02-14 00:49:16 +0000

    more changes ☄️

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,4 +1,4 @@
-import { IconBrandLinkedin, IconMail } from '@tabler/icons-react'
+import { IconAlignBoxBottomRight, IconArrowUpRight, IconBrandGmail, IconBrandLinkedin, IconBrandTwitter, IconBrandX, IconBriefcase, IconClipboardData, IconFileText, IconInbox, IconMail } from '@tabler/icons-react'
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Link from 'next/link'
@@ -23,16 +23,26 @@ const About: NextPage = () => {
 						<img src="/aaryan.jpeg" className="w-full h-auto object-center object-cover" />
 					</div>
 					<div className="flex flex-col lg:justify-end text-sm lg:text-base lg:w-1/2 h-full">
-						<div className="lg:max-w-3xl my-4 lg:mb-0">
+						<div className="hidden lg:flex lg:items-center lg:gap-4">
+							<div>
+								<img src="/ak[full].svg" className="z-10 text-white mix-blend-difference h-12" />
+							</div>
+							<div>
+								<img src="/arrow.svg" className="z-10 text-white mix-blend-difference h-10 mb-1 mr-1" />
+							</div>
+						</div>
+						<div className="text-justify lg:mt-8 lg:-mb-1 lg:max-w-4xl my-4">
 							Hello there! I am Aaryan. Aaryan Kapoor. I am a creative brand &lsquo;artisan&lsquo; and an adult fan of LEGO (AFOL). I always like to build and fiddle with things and that&#39;s what led me to the world of design.
 							<br /><br />
-							I strongly believe that design has the power to change everything. This is why I have a true passion for crafting emotive (& sometimes crazy) brand experiences that forge powerful connections with target audiences. 
-							<br /><br />
+							I strongly believe that design has the power to change everything. This is why I have a true passion for crafting emotive (& sometimes crazy) brand experiences that forge powerful connections with target audiences.
 							I leverage exceptional design skills across platforms like Adobe Illustrator, Photoshop, After Effects and Figma to develop visually compelling brand assets. 
 							I pair this with strong technical expertise in frontend development using tools like CSS, TypeScript, JavaScript, and ReactJS.
-							<div className="flex gap-4 mt-5 font-medium my-4">
+							<div className="flex gap-4 mt-5 font-medium my-4 text-xs lg:text-sm">
 								<Link href="https://www.linkedin.com/in/hiaaryan/" target='_blank' className="!cursor-pointer flex items-center">
-									<IconBrandLinkedin size={30} stroke={2} className="-ml-1.5 mr-2" /> Get in Touch
+									<IconBrandLinkedin size={25} stroke={2} className="-ml-1.5 mr-2" /> LinkedIn
+								</Link>
+								<Link href="https://www.linkedin.com/in/hiaaryan/" target='_blank' className="!cursor-pointer flex items-center">
+									<IconInbox size={25} stroke={2} className="mr-2" /> Resume
 								</Link>
 							</div>
 							I love collaborating with risk taking brands and agencies to create unique and memorable brand experiences. 
--- pages/index.tsx
+++ pages/index.tsx
@@ -11,28 +11,31 @@ const Home: NextPage = () => {
 				content="🙋🏻‍♂️ hey there, i am aaryan. i am a creative brand artisan specializing in design &amp; experience." />
 			</Head>
 			<div className="select-none font-mono bg-white">
-				<Link href="/about" className="z-30 flex items-start mix-blend-difference fixed top-0 left-0">
-					<img src="/ak.svg" className="!cursor-pointer z-10 mix-blend-difference m-4 h-6 lg:h-9" />
+				<Link href="/about" className="z-30 flex flex-col gap-2 mix-blend-difference fixed m-4 top-0 left-0">
+					<img src="/ak.svg" className="!cursor-pointer z-10 mix-blend-difference h-6 lg:h-9" />
 				</Link>
-				<div className="z-30 absolute flex gap-8 bottom-0 right-0 p-4 lg:mr-3 bg-white text-black">
-					<div className="text-sm lg:text-lg">
+				<div className="z-20 absolute w-screen justify-around flex bottom-0 right-0 p-4 bg-white text-black">
+					<div className="text-sm lg:text-base font-medium">
 						<Link href="/#work" className="!cursor-pointer flex items-center">
 							<img src="/arrow.svg" className="h-3 mr-2 mix-blend-difference" /> work
 						</Link>
 					</div>
-					<div className="text-sm lg:text-lg">
+					<div className="text-sm lg:text-base font-medium">
 						<Link href="/about" className="!cursor-pointer flex items-center">
 							<img src="/arrow.svg" className="h-3 mr-2 mix-blend-difference" /> about
+						</Link>
+					</div>
+					<div className="text-sm lg:text-base font-medium">
+						<Link href="mailto:hi.aaryankapoor@gmail.com" className="!cursor-pointer flex items-center">
+							<img src="/arrow.svg" className="h-3 mr-2 mix-blend-difference" /> email
 						</Link>
 					</div>
 				</div>
 				<div className="select-none font-mono h-screen w-auto">
 					<div className="relative flex justify-right items-end w-full h-full">
-						<div>
-							<video className="z-20 absolute top-0 right-0 h-full w-5/6 lg:w-4/5 -mt-10 lg:-mt-5" autoPlay muted playsInline loop>
-								<source src="/video.mp4" type="video/mp4" />
-							</video>
-						</div>
+						<video className="z-10 absolute top-0 right-0 -mt-10 lg:-mt-5 h-full w-5/6 lg:w-4/5" autoPlay muted playsInline loop>
+							<source src="/video.mp4" type="video/mp4" />
+						</video>
 					</div>
 				</div>
 				<div id="work" className="flex flex-col gap-4 p-4 select-none font-mono min-h-screen w-auto">
